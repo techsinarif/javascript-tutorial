e4c29b686b1d3dfeb19a0a62e90f32f3fb84b06e
techsinarif <techsinarif@gmail.com>
2021-06-21 21:00:26 +0530

    array methods

--- array-methods/index.js
+++ array-methods/index.js
@@ -16,6 +16,7 @@ const products = [
 // Syntax: filter((item, index, array)=>{}, thisArg) 
 
 const filteredItem = products.filter((product) => {
+
   return product.price > 600
 });
 console.log('Array.prototype.filter()');
@@ -25,10 +26,10 @@ console.log('======================');
 
 // Array.prototype.map(): Creates a new array populated with the results of calling a provided function on every element in the calling array.
 
-// Syntax: map((item, index, array)=>{}, thisArg)
+// Syntax: map((currentItem, index, array)=>{}, thisArg)
 
 const updatedProductPrice = products.map((product) => {
-  let products = {productId: product.productId, name: product.name, price: product.price * 2 };
+  const products = {productId: product.productId, name: product.name, price: product.price * 2 };
   return products;
 });
 console.log('Array.prototype.map()');
@@ -40,33 +41,36 @@ console.log('======================');
 // It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false. 
 // It doesn't modify the array.
 
-// Syntax: some((item, index, array)=>{}, thisArg)
+// Syntax: some((item, index, array)=>{}, thisArg);
+
 const isPriceAboveSixHundred = products.some((product) => product.price > 600);
 console.log('Array.prototype.some()');
-console.log('isPriceAboveSixHundred', isPriceAboveSixHundred);
+console.log('is there any one product above 600', isPriceAboveSixHundred);
 console.log('original products', products);
-console.log('======================');
+console.log('======================'); 
 
-// Array.prototype.every(): tests whether at least one element in the array passes the test implemented by the provided function. 
+// Array.prototype.every(): tests whether all the element in the array passes the test implemented by the provided function. 
 // It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false. 
 // It doesn't modify the array.
 
 // Syntax: every((item, index, array)=>{}, thisArg)
-const isPriceBelowFiveThousand = products.every((product) => product.price < 5000 );
-console.log('Array.prototype.every()');
-console.log('isPriceBelowFiveThousand', isPriceBelowFiveThousand);
+const isPriceBelowFiveThousand = products.every((product) => product.price > 600 );
+console.log('Array.prototype.every()'); 
+console.log('is all products are above 600', isPriceBelowFiveThousand);
 console.log('original products', products);
 console.log('======================');
 
-// Array.prototype.reduce(): executes a reducer function (that you provide) on each element of the array, resulting in a single output value.
+// Array.prototype.reduce(): executes a call back function on each element of the array, resulting in a single output value.
 
-// Syntax: reduce((accumulator, item, index, array)=>{}, initialValue)
+// Syntax: reduce((accumulator, currentItem, index, array)=>{}, initialValue)
 
 // accumulator: required => the initial value, or the previously returned value of the function
+// item: required => current item of the array.
+
 const arr = [1,2,3,4,5];
-const sum = arr.reduce((accumulator, item) => {
-  console.log(accumulator);
+const sum = arr.reduce((accumulator, item, index) => {
+  console.log(`iteration numer ${index} and the accumulator is ${accumulator}`);
   return accumulator + item;
-});
+}, 5);
 console.log('Array.prototype.reduce()');
-console.log('totalProductPrice', sum);
+console.log('Accumulated / total: ', sum); 
